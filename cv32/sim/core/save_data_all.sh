71d6d3d99423b9a11544d8b5af92f5819cb96ba2
lucafiore <45066316+lucafiore@users.noreply.github.com>
2021-02-15 15:00:22 +0100

    Update save_data_all.sh

--- cv32/sim/core/save_data_all.sh
+++ cv32/sim/core/save_data_all.sh
@@ -7,22 +7,21 @@ if [[ $# -ne 2 ]]; then
 else
 	stage=$1
 	direction=$2
+	arch=$3
 	for file in $(ls ./../../tests/programs/custom_FT/out | grep 'hex'); do
 		file_save=${file::-4}
 		if [[ $direction ==  "in" ]]; then
 			echo ""		
-			./comp_sim.sh -b asbv ref $file_save $stage save_data_in
-			##./comp_sim.sh -b asbv ft $file_save $stage save_data_in
+			./comp_sim.sh -b atsbv $arch $arch $file_save $stage save_data_in
 		else
 			if [[ $direction ==  "out" ]]; then
 				echo ""
-				./comp_sim.sh -b asbv ref $file_save $stage save_data_out -g
-				##./comp_sim.sh -b asbv ft $file_save $stage save_data_out -g
+				./comp_sim.sh -b atsbv $arch $arch $file_save $stage save_data_out -g
 			else 
 				if [[ $direction ==  "inout" ]]; then 
 					echo $file_save $stage
-					./comp_sim.sh -b asbv ref $file_save $stage save_data_in
-					./comp_sim.sh -b asbv ref $file_save $stage save_data_out -g
+					./comp_sim.sh -b atsbv $arch $arch $file_save $stage save_data_in
+					./comp_sim.sh -b atsbv $arch $arch $file_save $stage save_data_out -g
 				else
 					echo "wrong direction: only 'in', 'out' or ''inout."
 					echo "Usage: ./save_data_all.sh <stage_name> <in/out>"
